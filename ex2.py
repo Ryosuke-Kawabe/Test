19c6cc4d7a99f87054160b3f2840cd5bc54c469e
Ryosuke-Kawabe <gtw83427@nuc.kwansei.ac.jp>
2023-11-15 01:41:19 +0900

    ex2.pyの追加

--- ex2.py
+++ ex2.py
@@ -1,14 +1,22 @@
+import json
 
+with open('catalog.json', 'r') as f:
+    catalog_data = json.load(f)
 
-with open('catalog.json') as f:
-    lines = f.readlines()
+jacket_data = [item for item in catalog_data if item['name'] == 'jacket']
+
+if jacket_data:
     
-jacket = 0
-    
-for line in lines:
-    line = line.strip()
-        
-    if line.lstrip('-').isdigit(): 
-        sum+= int(line)
-            
-print(sum)
+    quantity = len(jacket_data)
+
+    prices = [item['price'] for item in jacket_data]
+
+    max = max(prices)
+
+    min = min(prices)
+
+    print("jacketの個数: {}".format(quantity))
+    print("jacketの最高価格: {}".format(max))
+    print("jacketの最低価格: {}".format(min))
+else:
+    print("データがありません")
