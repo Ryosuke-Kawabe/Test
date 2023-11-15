1d648a49cab4495914c2cc71ca8e0d07da390f6a
Ryosuke-Kawabe <gtw83427@nuc.kwansei.ac.jp>
2023-11-15 18:42:45 +0900

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,1 +1,16 @@
-print("a")
+import zipfile
+
+total= 0
+
+with zipfile.ZipFile("sample.zip", 'r') as zip_file:
+    file_list = zip_file.namelist()
+
+    for file_name in file_list:
+       
+        if file_name.endswith("_kgu.txt") and int(file_name.split("_")[1]) % 2 != 0:
+         
+            with zip_file.open(file_name) as file:
+                numbers = int(file.readline())
+                total += numbers
+
+print("Answer:", total)
